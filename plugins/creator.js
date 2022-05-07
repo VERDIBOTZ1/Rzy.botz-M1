210a7ecefc3c3c288b5b330f7f61d2c108eb52d7
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-08 03:18:32 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -16,7 +16,7 @@ async function handler(m) {
   let njid3 = number3 + '@s.whatsapp.net'
   let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
 
-let name4 = '@⳹ ❋ཻུ۪۪putra partner⳹ ❋ཻུ۪۪)'
+let name4 = '@⳹ ❋ཻུ۪۪pVERDI GANTENG🗿 ❋ཻུ۪۪)'
   number4 = owner[3].replace(/[^0-9]/g, '')
   let njid4 = number4 + '@s.whatsapp.net'
   let onW4 = await this.isOnWhatsApp(njid4) || { isBusiness: false }
